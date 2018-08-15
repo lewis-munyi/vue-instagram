5c6d7d9f57be62e91af51ad76e870c81bbf006f2
lewis-munyi <lewismunyi97@gmail.com>
2018-08-15 17:02:13 +0300

    App body and respective CSS

---
+++ src/components/AppBody.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-body">
+    Instagram app body! Here we'll show a scrollable view of the posts and like + comment bodies.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBody'
+}
+</script>
+
+<style lang="scss" src="../assets/styles/app-body.scss">
+  // Styles from stylesheet
+</style>
