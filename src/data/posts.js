a3804c8b105ba56fae02a0d45e0567c61c44ac41
lewis-munyi <lewismunyi97@gmail.com>
2018-08-15 16:51:11 +0300

    Dummy posts

---
+++ src/data/posts.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    username: 'slayqueen',
+    userImage: 'https://picsum.photos/300?image=1027',
+    postImage:
+      'https://picsum.photos/500/?random',
+    likes: 36,
+    hasBeenLiked: false,
+    caption: 'Random World...️',
+    filter: 'perpetua'
+  },
+  {
+    username: 'Lyon',
+    userImage: 'https://picsum.photos/300?image=1084',
+    postImage:
+      'https://picsum.photos/500?image=1005',
+    likes: 20,
+    hasBeenLiked: false,
+    caption: 'Views from the six...',
+    filter: 'clarendon'
+  },
+  {
+    username: 'cool_kid',
+    userImage:
+      'https://picsum.photos/300?image=996',
+    postImage:
+      'https://picsum.photos/500?image=1062',
+    likes: 49,
+    hasBeenLiked: false,
+    caption: 'Pup love <3',
+    filter: 'lofi'
+  }
+];
